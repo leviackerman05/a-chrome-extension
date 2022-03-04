dc2720c0be7f4280a7fac8abbc0badcdb83640bf
Priyansh Singh <singhpriyansh2000@gmail.com>
2022-03-04 20:50:19 +0530

    Leads are now saved to localstorage so they persist even after page refresh

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ let inputEl = document.getElementById("input-el");
 let inputBtn = document.getElementById("input-btn");
 let ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = " ";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
 });
 
